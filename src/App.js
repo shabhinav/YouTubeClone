f585d5c106c1c393fb43008d7cd631c3a5cbf1c9
shabhinav <d28.abhinav@gmail.com>
2020-05-17 00:17:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  async componentDidMount(){
+    let data = await axios.get("https://api.spotify.com/v1/tracks/{6are001c9z5muhk60v1ch05jr}", {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer BQDx7tRKinbXc_dG330Ahh6O6dkGevbpjBFxo2pNypRJ93ZDGf-FZf7bKQ9MhyUo2UncafY06S2K-q-sRTL_cdZERdQsuUFH1ZoxWpN9GFkudfpv8buzE7qXvYoV1lUDsGzB5p-XTy25YNa_snrRnGLvpDXBeFWsILyRV7txtw",
+        "Content-Type": "application/json"
+      }
+    })
+    console.log(data.data)
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
