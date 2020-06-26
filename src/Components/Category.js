066c8fa6da33fca6e0e8e2dab70b9a28d0499309
shabhinav <d28.abhinav@gmail.com>
2020-06-26 16:45:17 +0530

    cat revert

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,48 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
-import "./Category.scss";
-import done from "../Assests/done.svg";
-import * as actionCreators from "../store/action/index";
-import axios from "axios";
 
 class Category extends Component {
-  state = {
-    data: [],
-  };
-
-  async componentDidMount() {
-    let categoryid = this.props.match.params.category;
-    console.log("catid", categoryid);
-
-    let catdetails = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts/${categoryid}`
-    );
-    await this.setState({
-      data: catdetails,
-    });
-    console.log("data", this.state.data);
-  }
-
-  async componentDidUpdate(prevState) {
-    let categoryid = this.props.match.params.category;
-    console.log("catid", categoryid);
-    if (prevState.data !== this.state.data) {
-      let catdetails = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts/${categoryid}`
-      );
-      // this.setState({});
-      console.log("dtatatatatt", catdetails);
-    }
-
-    // this.setState({
-    //   data: categoryid,
-    // });
-  }
-
   render() {
-    console.log(this.props.match.params.category);
-    return <div className="category"></div>;
+    console.log("category", this.props.category);
+    return <div></div>;
   }
 }
 
